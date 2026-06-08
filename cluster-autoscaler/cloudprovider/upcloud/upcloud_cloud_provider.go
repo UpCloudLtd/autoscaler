88874b897811712ab783d1bdaf12e14d2bef45ef
Oleksandr Simonov <oleksandr@amoniac.eu>
2026-06-08 23:00:14 +0300

    chore(upcloud): restore provider registration, drop builder_upcloud.go

--- cluster-autoscaler/cloudprovider/upcloud/upcloud_cloud_provider.go
+++ cluster-autoscaler/cloudprovider/upcloud/upcloud_cloud_provider.go
@@ -25,14 +25,23 @@ import (
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/builder"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/upcloud/pkg/github.com/upcloudltd/upcloud-go-api/v8/upcloud/client"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/upcloud/pkg/github.com/upcloudltd/upcloud-go-api/v8/upcloud/service"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	coreoptions "k8s.io/autoscaler/cluster-autoscaler/core/options"
 	"k8s.io/autoscaler/cluster-autoscaler/utils/errors"
 	"k8s.io/autoscaler/cluster-autoscaler/utils/gpu"
+	"k8s.io/client-go/informers"
 	"k8s.io/klog/v2"
 )
+
+func init() {
+	builder.RegisterCloudProvider(cloudprovider.UpCloudProviderName, func(opts *coreoptions.AutoscalerOptions, do cloudprovider.NodeGroupDiscoveryOptions, rl *cloudprovider.ResourceLimiter, informerFactory informers.SharedInformerFactory) cloudprovider.CloudProvider {
+		return BuildUpCloud(opts, do, rl)
+	})
+	builder.SetDefaultCloudProvider(cloudprovider.UpCloudProviderName)
+}
 
 const (
 	timeoutProviderInit         time.Duration = time.Second * 15
